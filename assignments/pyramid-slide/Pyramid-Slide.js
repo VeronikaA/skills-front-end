400e4269c4c5c928f6b3b2e039b394384f851a3b
Perigree <holisticspiral@gmail.com>
2017-03-12 13:17:56 -0400

    Update Pyramid-Slide.js

--- assignments/pyramid-slide/Pyramid-Slide.js
+++ assignments/pyramid-slide/Pyramid-Slide.js
@@ -1,15 +1,15 @@
 function findHeightSymbol() {
 
-    // get the height the user typed upon input
+    // figure out the height the user typed
     heightStr = document.getElementById('height').value;
 
-    //  convert the string to an int
+    // here we convert the string to an int
     height = parseInt(heightStr);
 
     //set the range counter equal to the height
     document.getElementById("rangeHeight").innerHTML = height;
 
-    //get the symbol that was selected
+    //get the selceted symbol
     var symbol = document.getElementById("symbol").value;
 
     // draw the pyramid with the given height and selected symbol
@@ -31,7 +31,7 @@ function drawPyramid(height, symbol) {
         var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
             if (symbol == "@")
-                var spaceChar = "\xa0\xa0\xa0";  // this is how to code for spaces
+                var spaceChar = "\xa0\xa0\xa0"; // this is code to create spaces
             else {
                 spaceChar = "\xa0\xa0";
             }
@@ -41,50 +41,7 @@ function drawPyramid(height, symbol) {
             rowStr += symbol;
         }
 
-        textElem = document.createTextNode(rowStr);  // function findHeightSymbol() {
-
-    // get the height the user typed upon input
-    heightStr = document.getElementById('height').value;
-
-    //  convert the string to an int
-    height = parseInt(heightStr);
-
-    //set the range counter equal to the height
-    document.getElementById("rangeHeight").innerHTML = height;
-
-    //get the symbol that was selected
-    var symbol = document.getElementById("symbol").value;
-
-    // draw the pyramid with the given height and selected symbol
-    drawPyramid(height, symbol);
-}
-
-function drawPyramid(height, symbol) {
-
-    // print that pyramid!, but first clear out the old one
-    document.getElementById("pyramid").innerHTML = "";
-
-    for (var row = 0; row < height; row++) {
-
-        // figure out number of bricks and spaces
-        var numBricks = row + 2;
-        var numSpaces = height - row - 1;
-
-        // build up a string for this row
-        var rowStr = "";
-        for (var i = 0; i < numSpaces; i++) {
-            if (symbol == "@")
-                var spaceChar = "\xa0\xa0\xa0";  // this code creates spaces 
-            else {
-                spaceChar = "\xa0\xa0";
-            }
-            rowStr += spaceChar
-        }
-        for (var i = 0; i < numBricks; i++) {
-            rowStr += symbol;
-        }
-
-        textElem = document.createTextNode(rowStr); // must create an Element and also a textNode
+        textElem = document.createTextNode(rowStr);  // must create and Element and also a TextNode
 
                // create a <p> element with the text inside
         rowElem = document.createElement("p");
@@ -95,6 +52,3 @@ function drawPyramid(height, symbol) {
 
         }
 }
-
-
-    
